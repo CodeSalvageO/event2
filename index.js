99af0b54aefe9f2dd53c7583521760286792cda1
CodeSalvageO <40140954+CodeSalvageO@users.noreply.github.com>
2018-10-19 13:52:06 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+var user = [];
+var message = [];
+var userInput  = document.getElementById("user");
+var messageInput  = document.getElementById("message");
+function save() {
+ user.push(userInput.value);
+ message.push(messageInput.value);
+}
+
+ 
